aac7adeae288ede3669f377c43c35dad43828b74
Valeriu Coman <0comav27@solent.ac.uk>
2025-11-27 19:30:48 +0000

    add destination class

--- src/models/destination.kt
+++ src/models/destination.kt
@@ -1,2 +1,11 @@
 package models
 
+class destination(
+    var name: String = "",
+    var singlePrice: Double = 0.0,
+    var returnPrice: Double = 0.0,
+    var salesCount: Int = 0,
+    val activeOffers: MutableList<SpecialOffer> = mutableListOf()
+){
+
+}
