074914a978727230f4a109ac67ffa563d155023c
valeriu294 <0comav27@solent.ac.uk>
2025-12-02 16:02:03 +0000

    add id generating function

--- src/models/ticket.kt
+++ src/models/ticket.kt
@@ -1,2 +1,25 @@
 package models
 
+import java.time.LocalDateTime
+
+class Ticket (
+    var origin:String = "" ,
+    var destination:String = "" ,
+    var ticketType:String = "" ,
+    var price : Double = 0.0 ,
+    val purchaseDateTime : LocalDateTime = LocalDateTime.now(),
+    val ticketID : String = ""
+){
+    companion object {
+        private var ticketCounter = 1000
+        const val ORIGIN_STATION_NAME = "Central Station"
+}
+   //adding the generate ticket id function
+
+    fun generateTicketId(): String {
+        ticketCounter++
+        return "TKT-${ticketCounter}-${System.currentTimeMillis() % 10000}"
+    }
+}
+
+val ticketId: String = generateTicketId()
